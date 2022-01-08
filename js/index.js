84c0c17801c74493770f8fe68d5fc5ff37eb11db
garavito16 <lex_94@outlook.com>
2022-01-07 19:10:30 -0500

    subiendo cambios

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function clickLogueo(elemento) {
+    elemento.innerText = "Logout";
+}
+
+function clickDefinicion(elemento) {
+    elemento.remove();
+}
+
+function clickBotoncito(elemento) {
+    alert("Ninja Was Liked");
+}
